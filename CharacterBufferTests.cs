c0b7a2272ebf8c31360ec9b991e24060988abdcc
Jessica Mulein <jessica@mulein.com>
2021-12-02 11:12:44 -0800

    try to test more thoroughly

--- CharacterBufferTests.cs
+++ CharacterBufferTests.cs
@@ -2,6 +2,7 @@ using HACC.VirtualConsoleBuffer;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Collections.Generic;
 
 namespace HACC.Tests
 {
@@ -24,6 +25,10 @@ namespace HACC.Tests
             characterBuffer.WriteLine(line: expectedLineOne, new CharacterEffects(bold: true));
             characterBuffer.WriteLine(line: expectedLineTwo, new CharacterEffects());
 
+            var effectsPastEndLineOne = characterBuffer.CharacterEffectsAt(x: expectedLineOne.Length, y: 0);
+            var effectsPastEndLineTwo = characterBuffer.CharacterEffectsAt(x: expectedLineTwo.Length, y: 1);
+            var dirtySections = characterBuffer.DirtyRangeValues(includeEffectsChanges: true);
+
             Assert.AreEqual(
                 expected: expectedLineOne,
                 actual: characterBuffer.GetLine(x: 0, y: 0));
@@ -31,6 +36,21 @@ namespace HACC.Tests
             Assert.AreEqual(
                 expected: expectedLineTwo,
                 actual: characterBuffer.GetLine(x: 0, y: 1));
+
+            Assert.AreEqual(
+                expected: new List<(int xStart, int xEnd, int y, string value, CharacterEffects effects)>() {
+                    {( xStart: 0, xEnd: expectedLineOne.Length, y: 0, value: expectedLineOne, effects: new CharacterEffects(bold: true) )},
+                    {( xStart: 0, xEnd: expectedLineTwo.Length, y: 1, value: expectedLineOne, effects: new CharacterEffects(bold: false) )},
+                },
+                actual: dirtySections);
+
+            Assert.AreEqual(
+                expected: new CharacterEffects(),
+                actual: effectsPastEndLineOne);
+
+            Assert.AreEqual(
+                expected: new CharacterEffects(),
+                actual: effectsPastEndLineTwo);
         }
     }
 }
